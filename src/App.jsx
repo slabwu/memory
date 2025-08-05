48d63a4d56be63579e6e3268c484863fe85fb342
slabwu <134193743+slabwu@users.noreply.github.com>
2025-08-05 18:23:49 +0800

    Generate title colours

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import Content from './content.jsx'
 import './App.css'
 
 function App() {
+  let title = ['c', 'o', 'l', 'o', 'u', 'r']
+  let colour = title.map(letter => <span style={{color: getColour()}}>{letter}</span>)
+
   return (
     <>
       <header>
-        <h1>the colour game</h1>
+        <h1>the {colour} game</h1>
         <h2>Don't click any colour more than once! Can you get all eighteen?</h2>
       </header>
       <Content></Content>
@@ -14,3 +17,12 @@ function App() {
 }
 
 export default App
+
+function getColour() {
+    const hex = '0123456789ABCDE'
+    let output = '#'
+    for (let i = 0; i < 6; i++) {
+        output += hex.charAt(Math.floor(Math.random() * 15))
+    }
+    return output
+}
